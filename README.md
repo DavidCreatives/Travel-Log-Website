a993c4d0467bd2f06af5f956e2332a45803fc604
David Kamau <davidkamau.mec@gmail.com>
2026-02-13 23:03:27 +0300

    Create: destination constructor, some prototypes

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ A website that'll track and display the destinations a user has been.
 
 # Tests using TDD#
 ## Test 1 ##
-  Date and time: 9:51PM, 13/02/2026
-  Test: Creation of user
-  Status:✅ Passed  
-  Code changes: Creation of `travel` constructor
+  ** Date and time: ** 9.51PM, 13/02/2026
+  ** Test: ** Creation of user
+  ** Status: ** ✅ Passed  
+  ** Code changes: ** Creation of `travel` constructor
 
-
+## Test 2 ##
+  ** Date and time: **10.57PM, 13/02/2026 
+  ** Test: **Creation of destination constructor and some prototypes(genId and add new destination)
+  ** Status: **✅ Passed  
+  ** Code changes: **Creation of `generateID()`, `addDestination()` prototypes
--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 function travel(username) {
   this.destinations = {};
   this .travelId = 0;
-  message = "Welcome to your travel log, " + username + "!";
-  console.log(message);
+  // message = "Welcome to your travel log, " + username + "!";
+  // console.log(message);
+
+
+  travel.prototype.generateID = function() {
+    this.travelId++;
+    return this.travelId;};
+
+  travel.prototype.addDestination = function(name, location, landmarks, date, notes) {
+    const id = this.generateID();
+    this.destinations[id] = {
+      name: name,
+      location: location,
+      landmarks: landmarks,
+      date: date,
+      notes: notes
+    };
+  };
 };
 
-const travelLog = new travel("David");
-console.log(travelLog);
+function travelDestination(name, location, landmarks, date, notes) {
+  this.name = name;
+  this.location = location;
+  this.landmarks = landmarks;
+  this.date = date;
+  this.notes = notes;
+};
+
+// const newDestination = new travelDestination("Paris", "France", ["Eiffel Tower", "Louvre Museum"], "2023-05-01", "Had a great time exploring the city!");
+// const newTravel = new travel("David");
+// newTravel.addDestination(newDestination.name, newDestination.location, newDestination.landmarks, newDestination.date, newDestination.notes);
+// console.log(newTravel.destinations);
